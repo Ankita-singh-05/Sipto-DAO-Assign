08265af241ab144380e9ff85dd23d68d51b0778c
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-14 23:20:19 +0530

    Navbar Search Box

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,22 +3,28 @@ import "../../Styles/Navbar.css";
 import { BiSearch } from "react-icons/bi";
 
 function Navbar() {
-    return (
-        <>
-        <div className="navbar shadow p-3 mb-5 rounded bg-body d-flex justify-content-flex-end">
-            <div className ="links text-warning hover-zoom">
-                <a href="/home">Home</a>
-                <a href="/about">About</a>
-                <a href="/service">Service</a>
-                <a href="/contact">Contact</a>
-            </div>
-            <div className="flex gap-3 items-end bg-white rounded-md">
-                <BiSearch />
-                <input type="search" placeholder="Search"></input>
-            </div>
+  return (
+    <>
+      <div className="navbar shadow p-3 mb-5 rounded bg-body d-flex justify-content-flex-end">
+        <div className="links text-warning hover-zoom">
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+          <a href="/service">Service</a>
+          <a href="/contact">Contact</a>
         </div>
-        </>
-    );
-};
+        <div class="flex gap-3 rounded">
+          <input
+            type="search"
+            class="form-control rounded"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="search-addon"
+          />
+          <span class="border-0" id="search-addon"></span>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Navbar;
